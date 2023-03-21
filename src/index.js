1abb981cf2ec565509883a5850016b8088fa5523
Santosh-Konappanavar <siddukundaragi20@gmail.com>
2023-03-21 14:01:53 +0530

    ADD: display items on browser

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import './index.css';
+
+const movieList = document.querySelector('#movie');
+
+function getMealList() {
+  fetch('https://api.tvmaze.com/singlesearch/shows?q=girls&embed=episodes')
+    .then((response) => response.json())
+    .then((data) => {
+      if (data._embedded.episodes) {
+        data._embedded.episodes.forEach((movie) => {
+          document.querySelector('.list-count').innerHTML = movieList.children.length;
+          movieList.innerHTML += `
+            <div class="movie-item-wrapper" data-id="${movie.id}">
+              <div class="movie-item">
+                <div class="movie-img">
+                  <img src="${movie.image.medium}" alt="food">
+                </div>
+                <div class="movie-name">
+                  <div class="likeholder">
+                    <h3>${movie.name}</h3>
+                    <h5>Like</h5>
+                  </div>
+                  <a href="#" class="recipe-btn">Comments</a>
+                </div>
+              </div>
+            </div>
+          `;
+        });
+      } else {
+        movieList.innerHTML = 'Sorry we dont have your search';
+      }
+    });
+}
+document.addEventListener('DOMContentLoaded', () => {
+  getMealList();
+});
