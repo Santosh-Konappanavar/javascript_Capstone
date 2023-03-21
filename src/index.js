d8aa6e11e7db16c0d74532e2ff47cd8b0819e365
saluumaa <salumahassan02@gmail.com>
2023-03-21 15:30:27 +0300

    Change API

--- src/index.js
+++ src/index.js
@@ -3,24 +3,24 @@ import './index.css';
 const movieList = document.querySelector('#movie');
 /* eslint-disable no-underscore-dangle */
 function getMealList() {
-  fetch('https://api.tvmaze.com/singlesearch/shows?q=girls&embed=episodes')
+  fetch('https://api.tvmaze.com/shows')
     .then((response) => response.json())
-    .then((data) => {
-      if (data._embedded.episodes) {
-        data._embedded.episodes.forEach((movie) => {
+    .then((shows) => {
+      if (shows) {
+        shows.forEach((show) => {
           document.querySelector('.list-count').innerHTML = movieList.children.length;
           movieList.innerHTML += `
-            <div class="movie-item-wrapper" data-id="${movie.id}">
+            <div class="movie-item-wrapper" data-id="${show.id}">
               <div class="movie-item">
                 <div class="movie-img">
-                  <img src="${movie.image.medium}" alt="food">
+                  <img src="${show.image.medium}" alt="food">
                 </div>
                 <div class="movie-name">
                   <div class="likeholder">
-                    <h3>${movie.name}</h3>
+                    <h3>${show.name}</h3>
                     <h5>Like</h5>
                   </div>
-                  <a href="#" class="recipe-btn">Comments</a>
+                  <a href="#" class="comment-btn">Comments</a>
                 </div>
               </div>
             </div>
