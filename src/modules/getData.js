7ebe7886a302573b6da2018560997dfc54d96d08
saluumaa <salumahassan02@gmail.com>
2023-03-25 06:45:59 +0300

    add style to the navbar

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,5 +1,7 @@
-/* eslint-disable max-len */
-const movies = [1, 2, 3, 4, 55, 6, 93, 8, 22, 10, 11, 12, 13, 14, 15, 16, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 41, 42, 37, 38, 39, 40];
+const movies = [
+  1, 2, 3, 4, 55, 6, 93, 8, 22, 10, 11, 12, 13, 14, 15, 16, 18, 19, 20, 21, 22,
+  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 41, 42, 37, 38, 39, 40
+];
 const baseUrl = 'https://api.tvmaze.com';
 const showsUrl = '/shows/';
 const aPiLink = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi';
@@ -42,39 +44,27 @@ const hitlikesApi = async (url = '', id) => {
   return response.text();
 };
 
-const getMovies = async () => {
-  try {
-    const movielist = [];
-    for (let z = 0; z < movies.length; z += 1) {
-      const moviesId = baseUrl + showsUrl + movies[z];
-      const promise = getData(moviesId);
-      movielist.push(promise);
-    }
-    const movieData = await Promise.all(movielist);
-    return { success: true, movieData };
-  } catch (error) {
-    return { success: false, error };
+async function getMovies() {
+  const movielist = [];
+  for (let z = 0; z < movies.length; z += 1) {
+    const moviesId = baseUrl + showsUrl + movies[z];
+    const promise = getData(moviesId);
+    movielist.push(promise);
   }
-};
+  const movieData = await Promise.all(movielist);
+  return { success: true, movieData };
+}
 
-const getLikes = async () => {
-  try {
-    const postLikeLink = aPiLink + addLike;
-    const data = await getlikesCount(postLikeLink);
-    return { success: true, data };
-  } catch (error) {
-    return { success: false, error };
-  }
-};
+async function getLikes() {
+  const postLikeLink = aPiLink + addLike;
+  const data = await getlikesCount(postLikeLink);
+  return { success: true, data };
+}
 
-const postLikes = async (id) => {
-  try {
-    const postLikeLink = aPiLink + addLike;
-    const data = await hitlikesApi(postLikeLink, id);
-    return { success: true, data };
-  } catch (error) {
-    return { success: false, error };
-  }
-};
+async function postLikes(id) {
+  const postLikeLink = aPiLink + addLike;
+  const data = await hitlikesApi(postLikeLink, id);
+  return { success: true, data };
+}
 
 export { getMovies, getLikes, postLikes };
